6312af1cda4acad33f8f92d19775e55f331bf94c
GabrielLDA <gabriellima__2018@hotmail.com>
2019-04-09 22:18:58 -0300

    First commit

--- app/src/main/java/br/com/etecia/loginapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/loginapp/MainActivity.java
@@ -1,7 +1,12 @@
 package br.com.etecia.loginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button  btnLogar = findViewById(R.id.btnLogar);
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView edtLogin = findViewById(R.id.edtLogin);
+                TextView edtSenha = findViewById(R.id.edtSenha);
+                String username = edtLogin.getText().toString();
+                String password = edtSenha.getText().toString();
+
+                if(username.equals("etecia") && password.equals("etecia")){
+                    Intent intentHome = new Intent(MainActivity.this, Home.class);
+                    startActivity(intentHome);
+
+                }else{
+                    alert("Usuario ou Senha Invalidos");
+
+
+                }
+
+            }
+        });
+
+
+
     }
+    private void alert(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
